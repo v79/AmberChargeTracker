0f48101c5e1fe20c83e2cb4557b549c5adbedf73
v79 <liamjdavison@gmail.com>
2023-04-16 08:51:02 +0100

    Version number bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,40 +58,40 @@ android {
 dependencies {
 
     val composeBom = platform("androidx.compose:compose-bom:2022.10.00")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
+    implementation("androidx.compose:compose-bom:2023.04.00")
+    androidTestImplementation("androidx.compose:compose-bom:2023.04.00")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.0-beta02")
     implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.material:material-icons-extended")
 
     // images
-    implementation("androidx.activity:activity:1.6.1")
-    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("androidx.activity:activity:1.7.0")
+    implementation("io.coil-kt:coil-compose:2.3.0")
 
     // Hilt DI
-    implementation("com.google.dagger:hilt-android:2.44.2")
-    kapt("com.google.dagger:hilt-compiler:2.44.2")
+    implementation("com.google.dagger:hilt-android:2.45")
+    kapt("com.google.dagger:hilt-compiler:2.45")
 
     // room DB
-    implementation("androidx.room:room-ktx:2.4.3")
-    implementation("androidx.room:room-common:2.4.3")
-    kapt("androidx.room:room-compiler:2.4.3")
+    implementation("androidx.room:room-ktx:2.5.1")
+    implementation("androidx.room:room-common:2.5.1")
+    kapt("androidx.room:room-compiler:2.5.1")
 
     //testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 //    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
-    testImplementation("androidx.room:room-testing:2.4.3")
+    testImplementation("androidx.room:room-testing:2.5.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
 //    debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_version"
 
